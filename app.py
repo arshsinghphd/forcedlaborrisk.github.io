e09c23d9889cdf16622ec353aba7a63b7bdd833d
arshsinghphd <arshsinghphd@gmail.com>
2023-03-02 09:11:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import lookup
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
+from PIL import Image
 import re
 
 page_title = "Open Trade Data Pilot (Dummy)"
