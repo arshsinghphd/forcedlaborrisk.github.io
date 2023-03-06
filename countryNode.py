5a82644349ff67e4332aa04865505ab53e48a2a6
arshsinghphd <arshsinghphd@gmail.com>
2023-03-06 13:21:48 -0800

    Create countryNode.py

---
+++ countryNode.py
@@ -0,0 +1,11 @@
+class node:
+    def __init__(self, code, name, depth = -1, color = 'white', parent_code = 0):
+        self.code = code
+        self.name = name
+        self.depth = depth
+        self.color = color
+        self.parent = parent_code
+        self.trade_value = 0
+        self.imp_partners = []
+        self.trade_with_partners = 0
+        # white = unvisited, red = someone's imp trade partner
