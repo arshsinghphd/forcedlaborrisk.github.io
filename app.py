222ffa16cb030fd57c5db810ee7fdf611270b3be
arshsinghphd <arshsinghphd@gmail.com>
2023-03-19 11:14:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,49 +22,55 @@ st.image(logo)
 
 years = ["2021", "2020", "2019"]
 commodity = ["52 - Cotton"]
-trade = ['Import', 'Export', 'Both']
+trade = ['Export', 'Import', 'Both']
 areas = pd.read_csv('data/areas.csv')
 areas['name'] = areas['id'].astype(str)+ '-' + areas['text_x']
 areas = areas.set_index('id')
 areas = areas.rename(columns={'name' : 'text'})
 areas = areas[['text']]
-# -- Input Form --
+max_partners = 10
+max_levels = 10
 
+# -- Initiate session_state vars --
+if 'reporterName_raw' not in st.session_state:
+    st.session_state.reporterName_raw = '4-Afghanistan'    
+if 'year' not in st.session_state:
+    st.session_state.year = 2021
+if 'comm_code_raw' not in st.session_state:
+    st.session_state.comm_code_raw = '52 - Cotton'
+if 'imp_n' not in st.session_state:
+    st.session_state.imp_n = imp_n
+if 'levels_n' not in st.session_state:
+    st.session_state.levels_n = levels_n 
+
+# -- Input Form --
 with st.form("entry_form", clear_on_submit=False):
     st.write("In all of the selection boxes, you can choose from the options. You can also delete the default and start typing your choice and options will be suggested.")
-    reporterName = st.selectbox(f"Select Country",areas)
-    reporterCode = int(re.split('-',reporterName)[0])
-    reporterName = re.split('-',reporterName)[1]
+    reporterName_raw = st.selectbox(f"Select Country",areas)
     col1, col2 = st.columns(2)
     trade = col1.selectbox("Trade", trade)
     year = col1.selectbox("Year", years)
     comm_code_raw = col2.selectbox("HS Commodity Code",commodity)
-    comm_code = int(re.split('-',comm_code_raw)[0])
-    comm_name = re.split('-',comm_code_raw)[1]
+    
     "---"
     imp_n = st.number_input(f"Enter the number max number of trade partners.\nPartners with the largest trade values are chosen first.", \
-            min_value=1,max_value=10,format="%i",step=1)
+                            min_value=1,max_value=10,format="%i",step=1)
     levels_n = st.number_input("Depth: \n After your defined country, how many levels down do you want to search?", \
-    min_value=1, max_value= 5,format="%i",step=1)
+                            min_value=1, max_value=10,format="%i",step=1)
     dataDown = ''
     "---"
     submitted = st.form_submit_button()
-# --- Output Area ---
+
+# -- Based on Submission, update sesion_state variables --
 if submitted:
-    # fix commodity code with regex later
-    year = int(year)
-    imp_n = int(imp_n)
-    levels_n = int(levels_n)
-    st.write("You selected the following values.")
-    st.write("County: {}".format(reporterName))
-    st.write("Year: {}".format(year))
-    st.write("The max number of trade partners of each node country: {}".format(imp_n))
-    st.write("Search {} level(s) deep".format(levels_n))
-    "---"
+    st.session_state.reporterName_raw = reporterName_raw    
+    st.session_state.year = int(year)
+    st.session_state.comm_code_raw = comm_code_raw
+    st.session_state.imp_n = imp_n
+    st.session_state.levels_n = levels_n
 
-# for now overwriting commodity code as integer 52
-comm_code = 52
 
+# -- define functions before they are called --
 @st.cache_data
 def table_to_csv(df):
 # IMPORTANT: Cache the conversion to prevent computation on every rerun
@@ -79,27 +85,71 @@ def table_to_xls(df):
          header = ['Exporter(A)', 'Importer(B)', 'Export(A to B)*', 'Flag']
          ).encode('utf-8')
 
-# -- call the code --
+# -- Output Area --
+reporterName = re.split('-', st.session_state.reporterName_raw)[1]
+st.markdown("#### Current Values")
+st.write("County: {}".format(reporterName))
+st.write("Year: {}".format(year))
+st.write("The number of trade partners of each node country: {}.".format(imp_n))
+st.write("Search {} level(s) deep".format(levels_n))
+"---"
+st.header("Partnering Countries")
+st.write("Depending on your search, the names in the network graph below may not be legible, but you can zoom in and out. You can also hold the nodes and move them around to rearrange the map.")
+comm_name = re.split('-',st.session_state.comm_code_raw)[1]
+st.write("Red colored nodes: U. S. State dept. reports that {} grown and processed in that country to have high risk of involving forced or child labor. Any countries downstream a red node will also suffer the same risk.".format(comm_name))
+"---"
+# -- Adjust depth and partners --
+col1, col2= st.columns(2)
+col1.write('Adjust Levels')
+col2.write('Adjust Partners')
+col1, col2, col3, col4 = st.columns(4)
+# ---- Depth ----
+inc_level = col1.button('+1 Level')
+if inc_level:
+    if st.session_state.levels_n < 10:
+        st.session_state.levels_n += 1
+dec_level = col2.button('-1 Level')
+if dec_level:
+    if st.session_state.levels_n > 1:
+        st.session_state.levels_n -= 1
+levels_n = st.session_state.levels_n
+st.write("Search {} level(s) deep".format(levels_n))
+# ---- Partners ----
+inc_part = col3.button('+1 Partner')
+if inc_part:
+    if st.session_state.imp_n < 10:
+        st.session_state.imp_n += 1
+dec_p = col4.button('-1 Partner')
+if dec_p:
+    if st.session_state.imp_n > 1:
+        st.session_state.imp_n -= 1
+imp_n = st.session_state.imp_n
+st.write("The number of trade partners of each node country: {}.".format(imp_n))
+
+
+# -- lookup --
+#st.write(st.session_state)
+reporterCode = int(re.split('-', st.session_state.reporterName_raw)[0])
+year = st.session_state.year
+comm_code = int(re.split('-', st.session_state.comm_code_raw)[0])
+levels_n = st.session_state.levels_n # redundant but easy to read
+imp_n = st.session_state.imp_n # redundant but easy to read
+
 if imp_n**(levels_n + 1) > len(areas):
-        st.write("Your current selection results in too many countires. Please refine your search criteria by reducing partners or levels.")
+    st.markdown(" #### Your current selection results in too many countires.\n #### Please refine your search criteria by adjusting partners or levels.")
 else:
-    table = lookup.deep_search(reporterCode, year, comm_code, imp_n, levels_n)
-    # -- code has made an html file images/result.html --
-    st.header("Partnering Countries")
-    st.write("Depending on your search, the names in the network graph below may not be legible, but you can zoom in and out. You can also hold the nodes and move them around to rearrange the map.")
-    st.write("Red colored nodes: U. S. State dept. reports that {} grown and processed in that country to have high risk of involving forced or child labor. Any countries downstream a red node will also suffer the same risk.".format(comm_name))
+    # -- call lookup.py --         
+    table = lookup.deep_search(reporterCode, year, 
+                                comm_code, imp_n, levels_n)
+    # -- lookup.py has made an html file images/result.html --        
     HtmlFile = open("images/result.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read()
-    components.html(source_code, height=710, scrolling=True)
-    dataDown = st.radio("Would you like to download the underlying data as a file?",('No','Excel', 'CSV'))    
-        
+    components.html(source_code, height=410, scrolling=True)
+    # -- table download area --
+    dataDown = st.radio("Would you like to download the underlying data as a file?",('No','Excel', 'CSV'))        
     csv = table_to_csv(table)
     xls = table_to_xls(table)
-        
     if dataDown == 'Excel':
         st.download_button("Download Excel", xls, file_name='Table.xls', mime = 'xls', help = "Download file, may move the graph nodes around")
     if dataDown == 'CSV':
         st.download_button("Download CSV", csv, file_name='Table.csv', mime = 'csv', help = "Download file, may move the graph nodes around")
-
-# reset the image/result.html to default numbers    
-lookup.deep_search(reporterCode, year, comm_code, imp_n, 1)
