73f1edca86e9532fe4baae389479bf5526a2b0b0
arshsinghphd <arshsinghphd@gmail.com>
2023-03-20 13:31:06 -0700

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -7,13 +7,13 @@ import streamlit as st
 
 listfl = ['Georgia']
 
-def makePyvisGraph(node, pyvis_net, ncolor = 'white'):
+def makePyvisGraph(node, pyvis_net, flowCode, ncolor = 'white'):
+    # Node color
     if node.name in listfl:
-        ncolor = 'red'
-        
+        ncolor = 'red'    
     if node.name not in pyvis_net.get_nodes():
         pyvis_net.add_node(node.name, 
-                            title = 'Exporter', 
+                            title = node.name, 
                             color = ncolor, 
                             size = 12)
     else:
@@ -22,31 +22,54 @@ def makePyvisGraph(node, pyvis_net, ncolor = 'white'):
     if node.imp_partners:
         for partner in node.imp_partners:
             sum_trade += partner.trade_value
-            pyvis_net.add_node(partner.name, 
-                            title = 'Imports {:.2f}% of {}\'s export'
-                            .format(partner.trade_value, partner.parent), 
-                            color = ncolor,
-                            size = 12 - 2*partner.depth
-                            )
-            pyvis_net.add_edge(node.name, partner.name,
+            if flowCode == "X":
+                pyvis_net.add_node(partner.name, 
+                                title = '{} \n Imports {:.2f}% of {}\'s Supply'
+                                .format(partner.name, 
+                                        partner.trade_value, 
+                                        partner.parent), 
+                                color = ncolor,
+                                size = 12 - 2*partner.depth)
+                pyvis_net.add_edge(node.name, partner.name,
                         title = '{:.2f}%'.format(partner.trade_value))
-            if ncolor == "white":
-                makePyvisGraph(partner, pyvis_net)
-            else:
-                makePyvisGraph(partner, pyvis_net, ncolor)
-        pyvis_net.get_node(node.name)['title'] += \
+                if ncolor == "white":
+                    makePyvisGraph(partner, pyvis_net, flowCode)
+                else:
+                    makePyvisGraph(partner, pyvis_net, flowCode, ncolor)
+            elif flowCode == "M":
+                if partner.name in listfl:
+                    ncolor = 'red'
+                pyvis_net.add_node(partner.name, 
+                        title = '{} \n Supplies {:.2f}% of {}\'s Import'
+                        .format(partner.name, 
+                                partner.trade_value, 
+                                partner.parent),
+                        color = ncolor,
+                        size = 12 - 2*partner.depth)
+                pyvis_net.add_edge(partner.name, node.name, 
+                        title = '{:.2f}%'.format(partner.trade_value))
+                makePyvisGraph(partner, pyvis_net, flowCode)
+        if flowCode == "X":
+            pyvis_net.get_node(node.name)['title'] += \
                 '\n {:.2f}% of its exports go to following'.format(sum_trade)
-    
+        elif flowCode == "M":
+            pyvis_net.get_node(node.name)['title'] += \
+                '\n {:.2f}% of its imports come from the preceeding'.format(sum_trade)
     return 
     
-def deep_search(reporterCode, year, comm_codes, imp_n, levels_n):
+    
+def deep_search(reporterCode, year, comm_codes, flowCode, imp_n, levels_n):
     #BLOCK 1 
     # Based on names and codes in 'areas.csv', 
     # make a dict 'areas_nodes' of node objects keyed by code.
     areas = pd.read_csv('data/areas.csv', )
     areas.rename(columns={'text_x' : 'text'}, inplace=True)
     areas.set_index('id', inplace = True)
-    
+    # make a dict of names to codes
+    areas_nameTocode = {}
+    for i in areas.index:
+        areas_nameTocode[areas.loc[i]['text']] = i
+        
     tradeMat = pd.read_csv('data/tradeMat.csv')
     tradeMat.set_index('id', inplace = True)
     tradeMat.head()
@@ -60,9 +83,14 @@ def deep_search(reporterCode, year, comm_codes, imp_n, levels_n):
             if j not in areas_nodes.keys():
                 areas_nodes[j] = countryNode.node(j, areas.loc[j]['text'])
     ####
-    
-    #BLOCK 2: 
-    # TRADE CALCULATIONS AND BULDING DATAFRAME
+    # BLOCK 2: TRADE CALCULATIONS AND BULDING DATAFRAME
+    ####
+    if flowCode == "X":
+        table = pd.DataFrame(columns = ['a', 'b', 'export', 'flag'])
+    elif flowCode == "M":
+        table = pd.DataFrame(columns = ['a', 'b', 'import', 'flag'])
+    else:
+        table = pd.DataFrame(columns = ['a', 'b', 'import', 'export', 'flag'])
     # For level 1 iteration,
     # Start with the user provided country as the only node in 'curr_list'.
     # Make a list 'trade', of all nodes in areas_nodes it exports to.
@@ -71,8 +99,7 @@ def deep_search(reporterCode, year, comm_codes, imp_n, levels_n):
     # Scan this list to add important node attributes.
     # Save these nodes as the 'next_list'.
     # 'next_list' becomes 'curr_list' for the next level iteration.
-    # Iterate levels_n times.
-    table = pd.DataFrame(columns = ['a', 'b', 'export', 'flag'])
+    # Iterate levels_n times.   
     curr_list = [areas_nodes[reporterCode]]
     level = 1
     index = 1
@@ -109,34 +136,49 @@ def deep_search(reporterCode, year, comm_codes, imp_n, levels_n):
                     partner.depth = level
                     
                     flag = partner.color == 'red'
-                    exp = str(round(partner.trade_value, 2))[:4]
-                    table.loc[index] = pd.Series({'a':country.name, 
-                                          'b':partner.name, 
-                                          'export':exp,
-                                          'flag': flag})
+                    if flowCode == "X":
+                        exp = str(round(partner.trade_value, 2))[:4]
+                        table.loc[index] = pd.Series({'a':country.name, 
+                                              'b':partner.name, 
+                                              'export':exp,
+                                              'flag': flag})
+                    if flowCode == "M":
+                        imp = str(round(partner.trade_value, 2))[:4]
+                        table.loc[index] = pd.Series({'a':country.name, 
+                                              'b':partner.name, 
+                                              'import':imp,
+                                              'flag': flag})
                     index += 1
             country.trade_with_partners = sum_trade*100/tot_trade
             next_list.extend(country.imp_partners)
         curr_list = next_list
         level += 1
-    table.loc['*'] = pd.Series({'a':'As % of total exports of B'})
+    if flowCode == "X":
+        table.loc['*'] = pd.Series({'a':'As % of total exports of B'})
+    elif flowCode == "M":
+        table.loc['*'] = pd.Series({'a':'As % of total imports of B'})
     ####
-    
-    #BLOCK 3
+    #BLOCK 3: MAKE GRAPH AND SAVE AS HTML
+    ####
     # Make a pyvis graph and save it as an html
-    
-    #pyvis_net = Network(height="700px", width="100%", 
-    #                    bgcolor="#222222", font_color="white", 
-    #                    directed=True)
     pyvis_net = Network(height="400px", width="100%", 
                         bgcolor="#222222", font_color="white", 
                         directed=True)
-    makePyvisGraph(areas_nodes[reporterCode], pyvis_net)
+    makePyvisGraph(areas_nodes[reporterCode], pyvis_net, flowCode)
+    if flowCode == 'M':
+        for node in pyvis_net.get_nodes():
+            if pyvis_net.get_node(node)['color'] == 'red':
+                node_code = areas_nameTocode[node]
+                parent = areas_nodes[node_code].parent
+                while parent != 0:
+                    pyvis_net.get_node(parent)['color'] = 'red'
+                    node = parent
+                    node_code = areas_nameTocode[node]
+                    parent = areas_nodes[node_code].parent
+    ####    
     pyvis_net.write_html("images/result.html", local=True)
-    ####
-    
     return table
     
     
 if __name__ == '__main__':
-    deep_search(reporterCode,year,comm_codes,imp_n,levels_n)
+    deep_search(reporterCode,year,comm_codes,flowCode,imp_n,levels_n)
