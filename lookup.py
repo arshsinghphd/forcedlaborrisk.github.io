36e7789a11397e10f06e3d62dfe886506a56e49d
arshsinghphd <arshsinghphd@gmail.com>
2023-03-27 11:16:55 -0700

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -1,11 +1,12 @@
-import countryNode
+import countryNode  
 
+import numpy as np
 import pandas as pd
 from pyvis.network import Network
 import requests 
 import streamlit as st
 
-listfl = ['Georgia']
+listfl = list(pd.read_csv('data/listfl_cotton.csv')['0'])
 
 def makePyvisGraph(node, pyvis_net, flowCode, ncolor = 'white'):
     # Node color
@@ -56,12 +57,53 @@ def makePyvisGraph(node, pyvis_net, flowCode, ncolor = 'white'):
             pyvis_net.get_node(node.name)['title'] += \
                 '\n {:.2f}% of its imports come from the preceeding'.format(sum_trade)
     return 
-    
+
+@st.cache_data
+def make_mat(year, comm_codes, flowCode):
+    df = pd.read_csv('data/{}_{}_{}.csv'.format(flowCode, comm_codes, year), encoding = 'cp437')
+    df = df[['ReporterCode','PartnerCode','PrimaryValue']]
+    ids = list(df['ReporterCode'].unique())
+    temp = np.zeros(shape=(len(ids),len(ids)), dtype = 'int64')
+    for i in range(len(ids)):
+        for j in range(len(ids)):
+            if i == j:
+                temp[i][j] = 0
+            else:
+                try:
+                    temp[i][j] = df[df['ReporterCode'] == ids[i]][df['PartnerCode'] == ids[j]]['PrimaryValue']/10**6
+                except:
+                    temp[i][j] = 0
+    tradeMat = pd.DataFrame(temp)
+    tradeMat.index.name = 'id'
+    colsToIds = {}
+    for i, j in zip(range(0, len(ids)), ids):
+        colsToIds[i] = j
+    tradeMat.rename(colsToIds, inplace = True)
+    tradeMat.rename(columns = colsToIds, inplace = True)
+    tradeMat = tradeMat.astype(int)
+    return tradeMat, ids
     
 def deep_search(reporterCode, year, comm_codes, flowCode, imp_n, levels_n):
+    ####
     #BLOCK 1 
+    ####
+    # Based on the dat from UN Comtrade, make tradeMat
+    # If there is not data for reporterCode in the tradeMat, return None already
+    
+    # tradeMat = pd.read_csv('data/tradeMat.csv')
+    # tradeMat.set_index('id', inplace = True)
+    # tradeMat.head()
+    
+    tradeMat, ids = make_mat(year, comm_codes, flowCode)
+    if reporterCode not in ids:
+        return False, 0
+    
+    ####
+    #BLOCK 2 
+    ####
     # Based on names and codes in 'areas.csv', 
     # make a dict 'areas_nodes' of node objects keyed by code.
+    
     areas = pd.read_csv('data/areas.csv', )
     areas.rename(columns={'text_x' : 'text'}, inplace=True)
     areas.set_index('id', inplace = True)
@@ -69,21 +111,25 @@ def deep_search(reporterCode, year, comm_codes, flowCode, imp_n, levels_n):
     areas_nameTocode = {}
     for i in areas.index:
         areas_nameTocode[areas.loc[i]['text']] = i
-        
-    tradeMat = pd.read_csv('data/tradeMat.csv')
-    tradeMat.set_index('id', inplace = True)
-    tradeMat.head()
-
     areas_nodes = {}
+        
     for j in tradeMat.index:
         if j == reporterCode:
             if j not in areas_nodes.keys():
-                areas_nodes[j] = countryNode.node(j, areas.loc[j]['text'], 0)
+                if j == 380: # italy code has changed
+                    areas_nodes[j + 1] = countryNode.node(j + 1, areas.loc[j]['text'], 0)
+                else:
+                    areas_nodes[j] = countryNode.node(j, areas.loc[j]['text'], 0)
         else:
             if j not in areas_nodes.keys():
-                areas_nodes[j] = countryNode.node(j, areas.loc[j]['text'])
+                if j == 380: # italy code has changed from 2021 to 2022
+                    areas_nodes[j] = countryNode.node(j, areas.loc[j + 1]['text'], 0)
+                else:
+                    areas_nodes[j] = countryNode.node(j, areas.loc[j]['text'])
+    
+    
     ####
-    # BLOCK 2: TRADE CALCULATIONS AND BULDING DATAFRAME
+    # BLOCK 3: TRADE CALCULATIONS AND BULDING DATAFRAME
     ####
     if flowCode == "X":
         table = pd.DataFrame(columns = ['a', 'b', 'export', 'flag'])
@@ -115,41 +161,45 @@ def deep_search(reporterCode, year, comm_codes, flowCode, imp_n, levels_n):
             tot_trade = 0
             for j in areas_nodes.values():
                 if not j.engaged:
-                    tv = tradeMat.loc[i, str(j.code)]
-                    j.trade_value = tv
-                    trade.append(j)
-                    tot_trade += tv
+                    #tv = tradeMat.loc[i, str(j.code)]
+                    tv = tradeMat.loc[i, j.code]
+                    if tv > 0:
+                        j.trade_value = tv
+                        trade.append(j)
+                        tot_trade += tv
             trade.sort(key = lambda x: x.trade_value, reverse=True)
-            
-            trade = trade[:imp_n]
+            idx_trade = min(len(trade), imp_n)
+            trade = trade[:idx_trade]
             sum_trade = 0
             counter = 0
-            for partner in trade:
-                counter += 1
-                if not partner.engaged:
-                    sum_trade += partner.trade_value
-                    partner.trade_value = 100*partner.trade_value/tot_trade
-                    country.imp_partners.append(partner)
-                    partner.parent = country.name
-                    partner.color = country.color
-                    partner.engaged = True
-                    partner.depth = level
-                    if flowCode == "X":
-                        flag = partner.color == 'red'
-                        exp = str(round(partner.trade_value, 2))[:4]
-                        table.loc[index] = pd.Series({'a':country.name, 
-                                              'b':partner.name, 
-                                              'export':exp,
-                                              'flag': flag})
-                    if flowCode == "M":
-                        imp = str(round(partner.trade_value, 2))[:4]
-                        table.loc[index] = pd.Series({'a':country.name, 
-                                              'b':partner.name, 
-                                              'import':imp,
-                                              'flag':False})
-                    index += 1
-            country.trade_with_partners = sum_trade*100/tot_trade
-            next_list.extend(country.imp_partners)
+            if len(trade) > 0:
+                for partner in trade:
+                    counter += 1
+                    if not partner.engaged:
+                        sum_trade += partner.trade_value
+                        partner.trade_value = 100*(partner.trade_value/tot_trade)
+                        country.imp_partners.append(partner)
+                        partner.parent = country.name
+                        partner.color = country.color
+                        partner.engaged = True
+                        partner.depth = level
+                        if flowCode == "X":
+                            flag = partner.color == 'red'                            
+                            exp = str(round(partner.trade_value, 2))[:4]
+                            table.loc[index] = pd.Series({'a':country.name, 
+                                                  'b':partner.name, 
+                                                  'export':exp,
+                                                      'flag': flag})
+                        if flowCode == "M":
+                            imp = str(round(partner.trade_value, 2))[:4]
+                            table.loc[index] = pd.Series({'a':country.name, 
+                                                      'b':partner.name, 
+                                                      'import':imp,
+                                                      'flag':False})
+                        index += 1
+            if tot_trade > 0:
+                country.trade_with_partners = (sum_trade/tot_trade)*100
+                next_list.extend(country.imp_partners)
         curr_list = next_list
         level += 1
     if flowCode == "X":
@@ -157,8 +207,9 @@ def deep_search(reporterCode, year, comm_codes, flowCode, imp_n, levels_n):
     elif flowCode == "M":
         table.loc['*'] = pd.Series({'a':'As % of total imports of A'})
     
+    
     ####
-    #BLOCK 3: MAKE GRAPH AND SAVE AS HTML
+    #BLOCK 4: MAKE GRAPH AND SAVE AS HTML
     ####
     # Make a pyvis graph and save it as an html
     pyvis_net = Network(height="400px", width="100%", 
@@ -179,7 +230,8 @@ def deep_search(reporterCode, year, comm_codes, flowCode, imp_n, levels_n):
                     parent = areas_nodes[node_code].parent
     ####    
     pyvis_net.write_html("images/result.html", local=True)
-    return table
+    
+    return True, table
     
     
 if __name__ == '__main__':
